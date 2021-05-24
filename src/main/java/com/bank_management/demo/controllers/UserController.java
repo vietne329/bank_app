87a39707f3113576bda8a543e30c72641ad2fd2f
vietne329 <mikedukeviet@gmail.com>
2021-05-24 21:06:34 +0700

    update user

--- src/main/java/com/bank_management/demo/controllers/UserController.java
+++ src/main/java/com/bank_management/demo/controllers/UserController.java
@@ -2,15 +2,15 @@ package com.bank_management.demo.controllers;
 
 
 import com.bank_management.demo.entities.User;
+import com.bank_management.demo.respositories.UserRepository;
 import com.bank_management.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,7 +18,8 @@ public class UserController {
 
     @Autowired
     UserService userSer;
-
+    @Autowired
+    UserRepository userRepository;
     @GetMapping(value = "/user", produces = "application/json")
     public ResponseEntity<?> getUserById(@RequestParam("id") Long id){
         User user = userSer.getUserById(id);
@@ -26,5 +27,20 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if (userOptional.isPresent()) {
+            User _tutorial = userOptional.get();
+            _tutorial.setFacebookId(user.getFacebookId());
+            _tutorial.setRoles(user.getRoles());
+            System.out.println(_tutorial.getRoles().toString());
+            return new ResponseEntity<>(userRepository.save(_tutorial), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
